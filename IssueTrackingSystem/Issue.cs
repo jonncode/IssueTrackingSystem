d0869640bb0892691e9df853b5cc8054c90bf2b3
Jonathan Eriksson <LandisGaming@hotmail.com>
2019-10-21 02:15:37 +0200

    Add project files.

---
+++ IssueTrackingSystem/Issue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTrackingSystem
+{
+    class Issue
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public string Assignee { get; set; }
+        public int Priority { get; set; }
+    }
+}
